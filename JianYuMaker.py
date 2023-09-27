a184e0ebb81acdda79609a2b628828ca8ec1bd64
MSI\user <hsuchen1022@gmail.com>
2023-09-28 00:38:51 +0800

    main program file initial commit

---
+++ JianYuMaker.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import json
+from pprint import pprint
+from ArticutAPI import Articut
+
+with open("account.info", encoding="utf-8") as f:
+    accountDICT = json.load(f)
+articut = Articut(username=accountDICT["username"], apikey=accountDICT["api_key"])
+
+#簡語化函數
+def jianyu(inputSTR):
+    jianyu_result = ""
+    
+    resultDICT= articut.parse(inputSTR, level="lv2")['result_obj']
+    
+    
+    for i in range(len(resultDICT)):
+        
+        for j in range(len(resultDICT[i])):
+            if (len(resultDICT[i])) == 1:
+                if (len(resultDICT[i][j]['text']) == 4):
+                    jianyu_result = jianyu_result + resultDICT[i][j]['text'][0] + resultDICT[i][j]['text'][2]
+                    break
+            else:
+                if resultDICT[i][j]['pos'] in ['CLAUSE_AnotAQ', 'MODIFIER', 'ENTITY_DetPhrase', 'DegreeP', 'TIME_justtime']:
+                    jianyu_result = jianyu_result + resultDICT[i][j]['text']
+                    
+                else:
+                    if j == 0:
+                        if 'ENTITY' in resultDICT[i][j]['pos']:
+                            jianyu_result = jianyu_result + resultDICT[i][j]['text']
+                        elif 'ACTION' in resultDICT[i][j]['pos']:
+                            jianyu_result = jianyu_result + resultDICT[i][j]['text']
+                            
+                    else:
+                        jianyu_result = jianyu_result + resultDICT[i][j]['text'][0]
+            
+    print ("\n" + jianyu_result + "\n")
+    
+
+#程式進入點
+if __name__ == "__main__":
+    resultDICT = []
+    while True:
+        inputSTR = input("請輸入你要轉換成簡語的句子:\n")
+        
+        jianyu(inputSTR)
+    
+        
