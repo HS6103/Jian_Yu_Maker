e5d1580b4c313432005c5b995df176465be7464c
HS6103 <hsuchen1022@gmail.com>
2024-02-10 00:02:19 +0800

    added jquery, working on ajax, modified app.py/index.html/style.css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import json
 from JianYuMaker import jianyu
 
@@ -6,11 +6,15 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    return render_template("index.html")
+
+@app.route("/get_result", methods=["POST"] )
+def get_result():
     if request.method == "POST":
         user_input = request.form["user_input"]
         resultSTR = jianyu(user_input)
-        return f"Here's your jianyu result: {resultSTR}"
-    return render_template("index.html")
+    return jsonify(f"Here's your jianyu result: {resultSTR}")    
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
