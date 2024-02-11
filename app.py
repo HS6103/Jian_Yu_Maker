8dc195db86ee3a4770cde08ce5b10cab54492d12
HS6103 <hsuchen1022@gmail.com>
2024-02-11 19:10:59 +0800

    test solution

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ def jian_string(inputSTR):
 
 @app.route("/", methods=['GET','POST'])
 def index():
+    if request.method == 'POST':
+        user_input = request.form.get('user_input')
+        if user_input:
+            result = jian_string(user_input)
+            return jsonify(result=result)
+        else:
+            return jsonify(result='Input needed')        
+        
     return render_template("index.html")
-
-@app.route('/get_result', methods=['GET', 'POST'] )
-def get_result():
-    user_input = request.get_json["user_input"]
-    result = jian_string(user_input)
-    return jsonify(mutated_string=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
